077aca14a8173cdbe9b1ada10ef90946bf272cec
Joanna-Golofit <sunderlajsa@gmail.com>
2021-11-16 13:41:07 +0100

    dodaja sie elementy listy z buttonami i jest przekreślenie

--- index.js
+++ index.js
@@ -7,11 +7,19 @@ const input = dqs("#task-title");
 const addItem = (e) => {
   e.preventDefault();
   const text = input.value;
+  if (!text || text === " ") {
+    alert("wpisz cos");
+    return;
+  };
   const newItem = document.createElement("li");
+  const newBtnClose = document.createElement("button");
 
   newItem.innerText = text;
   ul.appendChild(newItem);
-  console.log("dupa");
+  newBtnClose.classList.add("btn", "btn-sm", "btn-warning");
+  newBtnClose.innerText = "x";
+  console.log(newBtnClose);
+  newItem.append(newBtnClose);
   // console.log(ul);
   // console.log(text);
   // console.log(newItem);
@@ -22,8 +30,14 @@ const removeItem = (e) => {
   if (e.target.nodeName !== "BUTTON") return;
   e.target.closest("li").remove();
 };
+const crossOutItem = (e) => {
+  console.log(e.target.nodeName);
+  if (e.target.nodeName !== "LI") return;
+  e.target.classList.toggle("done");
+}
 
 // console.log(button);
 button.addEventListener("click", addItem);
 ul.addEventListener("click", removeItem);
+ul.addEventListener("click", crossOutItem);
 // addItem();
